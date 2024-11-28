116478fe1556472a17ab3b9b6297b169ec5abcc7
Italo Matias <italoba.leite@gmail.com>
2024-11-28 14:45:44 -0300

    adicionando try except

--- osImport.py
+++ osImport.py
@@ -1,14 +1,17 @@
 import os
 
 caminho = "C:\\Users\\Italo\\OneDrive\\Documentos\\Pessoal\\comandos e anotações\\git comandos.txt"
-with open(caminho) as arquivo:
+try:
+    with open(caminho) as arquivo:
 
-    if os.path.exists(caminho):
-        print("esse local existe")
-        if os.path.isfile(caminho):
-            print("Isso é um arquivo")
-            print(arquivo.read())
-        elif os.path.isdir(caminho):
-            print("isso é uma pasta")
-    else:
-        print("esse local não existe")
+        if os.path.exists(caminho):
+            print("esse local existe")
+            if os.path.isfile(caminho):
+                print("Isso é um arquivo")
+                print(arquivo.read())
+            elif os.path.isdir(caminho):
+                print("isso é uma pasta")
+        else:
+            print("esse local não existe")
+except Exception as e:
+    print(e)
