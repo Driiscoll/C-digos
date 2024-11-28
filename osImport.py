a2c39de553e3eed1efc2e966b1f13f3651af0079
Italo Matias <italoba.leite@gmail.com>
2024-11-28 14:27:40 -0300

    abrir o arquvivo no terminal

--- osImport.py
+++ osImport.py
@@ -1,12 +1,14 @@
 import os
 
 caminho = "C:\\Users\\Italo\\OneDrive\\Documentos\\Pessoal\\comandos e anotações\\git comandos.txt"
+with open(caminho) as arquivo:
 
-if os.path.exists(caminho):
-    print("esse local existe")
-    if os.path.isfile(caminho):
-        print("Isso é um arquivo")
-    elif os.path.isdir(caminho):
-        print("isso é uma pasta")
-else:
-    print("esse local não existe")
+    if os.path.exists(caminho):
+        print("esse local existe")
+        if os.path.isfile(caminho):
+            print("Isso é um arquivo")
+            print(arquivo.read())
+        elif os.path.isdir(caminho):
+            print("isso é uma pasta")
+    else:
+        print("esse local não existe")
