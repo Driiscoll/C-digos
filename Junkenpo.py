7c103a3168feb976ad052e67d86cc8baebb7d994
Italo Matias <italoba.leite@gmail.com>
2024-11-29 15:53:43 -0300

    aprimorando o jukenpo

--- Junkenpo.py
+++ Junkenpo.py
@@ -1,22 +1,42 @@
-import random
+from random import choice
+valor = True
+placarPlayer = 0
+placarCpu = 0
 
-cpu = ["papel", "pedra", "tesoura"]
+while valor == True:
+    try:
+        cpu = ["papel", "pedra", "tesoura"]
+        player = input("pedra, papel ou tesoura: ")
+        cpu = choice(cpu)
 
-player = input("pedra, papel ou tesoura: ")
+        print("a CPU escolheu: ", cpu)
+        if cpu == "pedra" and player == "papel":
+            print("Jogador Venceu!!")
+            placarPlayer += 1
+        elif cpu == "pedra" and player == "tesoura":
+            print("CPU Venceu")
+            placarCpu += 1
+        elif cpu == "papel" and player == "pedra":
+            print("CPU Venceu")
+            placarCpu += 1
+        elif cpu == "papel" and player == "tersoura":
+            print("Jogador Venceu!!")
+            placarPlayer += 1
+        elif cpu == "tesoura" and player == "papel":
+            print("CPU Venceu")
+            placarCpu += 1
+        elif cpu == "tesoura" and player == "pedra":
+            print("Jogador Venceu!!")
+            placarPlayer += 1
+        else:
+            print("jogo empatado")
 
-cpu = random.choice(cpu)
-
-if cpu == "pedra" and player == "papel":
-    print("Jogador Venceu!!")
-elif cpu == "pedra" and player == "tesoura":
-    print("CPU Venceu")
-elif cpu == "papel" and player == "pedra":
-    print("CPU Venceu")
-elif cpu == "papel" and player == "tersoura":
-    print("Jogador Venceu!!")
-elif cpu == "tesoura" and player == "papel":
-    print("CPU Venceu")
-elif cpu == "tesoura" and player == "pedra":
-    print("Jogador Venceu!!")
-else:
-    print("jogo empatado")
+        print("\nSegue placar do jogo:\nJogador: ",placarPlayer,"\nCPU: ",placarCpu)
+            
+        novamente = input("\nGostaria de jogar novamente?\n1 = sim\n2 = não\n")
+        if novamente == "1":
+            valor = True
+        else:
+            valor = False
+    except Exception as e:
+        print(e)
